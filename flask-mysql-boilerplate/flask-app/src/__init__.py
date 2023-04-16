ded8b3e8f79c7e5c396cccf5bf2085b4ffcf1165
nicholasb4711 <nicholasb4711@gmail.com>
2023-04-16 16:38:49 -0400

    Update __init__.py

--- flask-mysql-boilerplate/flask-app/src/__init__.py
+++ flask-mysql-boilerplate/flask-app/src/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'healthbasellc'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
@@ -27,12 +27,12 @@ def create_app():
     # Add a default route
     @app.route("/")
     def welcome():
-        return "<h1>Welcome to the 3200 boilerplate app</h1>"
+        return "<h1>Welcome to the 3200 HealthBase app</h1>"
 
     # Import the various routes
     from src.views import views
     from src.users import users
-    from src.foods  import foods
+    from src.foods import foods
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
