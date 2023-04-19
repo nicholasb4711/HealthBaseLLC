23732402f927c7b626bf43858e58ed3d1a190403
nicholasb4711 <nicholasb4711@gmail.com>
2023-04-19 17:51:26 -0400

    updates to WorkoutHistory

--- flask-mysql-boilerplate/flask-app/src/users.py
+++ flask-mysql-boilerplate/flask-app/src/users.py
@@ -26,6 +26,7 @@ def get_customers():
 def get_customer(userID):
     cursor = db.get_db().cursor()
     cursor.execute('select * from customers where id = {0}'.format(userID))
+    
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
--- flask-mysql-boilerplate/flask-app/src/workout_history.py
+++ flask-mysql-boilerplate/flask-app/src/workout_history.py
@@ -12,6 +12,21 @@ def get_allWorkoutHistory():
         SELECT EntryID, UserID, DateOfEntry, WorkoutID
         FROM WorkoutHistory
     '''
+    return get_Data(query)
+
+# Get customer detail for customer with particular userID
+@workout_history.route('/users-workout-history/<userID>', methods=['GET'])
+def get_customer(userID):
+    query = '''
+        SELECT *
+        FROM WorkoutHistory
+        WHERE UserID = 1
+    '''
+    #.format(userID)
+    return get_Data(query)
+
+# general function for retrieving data
+def get_Data(query):
     cursor = db.get_db().cursor()
     cursor.execute(query)
     json_data = []
@@ -24,20 +39,3 @@ def get_allWorkoutHistory():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
-
-# Get customer detail for customer with particular userID
-'''
-@users.route('/users/<userID>', methods=['GET'])
-def get_customer(userID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-'''
